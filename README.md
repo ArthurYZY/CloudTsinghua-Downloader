b392533781d71a811f38c71388c6221d7bd34ca9
Arthur Yu <arthuryzy329@gmail.com>
2020-01-02 11:26:01 +0800

    add usage to README.md

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # CloudTsinghua-Downloader
+
+**适用场景：** 共享文件夹，但是文件夹下文件众多，总体积多大，无法打包下载
+
+![](img/files.png)
+
+
+
+### 使用方法
+
+**环境：**  python3 (>= 3.5)
+
+1. 修改conf.json中的id，此id即为共享文件夹的id
+
+   e.g. ![](img/id.png)
+
+   地址栏红色框中涂掉部位即为id
+
+2. （Optional）配置conf文件中ignore项。
+
+   * 输入文件全名（包含路径）=> 该文件不会被下载
+
+   * 输入从根目录到指定目录的一个路径（需要以/结尾）=> 该目录下所有文件和目录均不会被下载
+
+3. 
+
+   ```
+   cd CloudTsinghua-Downloader
+   python3 download.py
+   
+   ```
+
+4. 下载文件会被存储在CloudTsinghua-Downloader/Downloads下
+
+ 
---
+++ download.py
@@ -0,0 +1,93 @@
+import requests
+import json
+import os
+
+with open("conf.json", 'r') as f:
+    content = json.load(f)
+    file_id = content['id']
+    ignore_ls = content['ignore']
+
+FILE_ID = file_id
+IGNORE_LS = ignore_ls
+URL = f"https://cloud.tsinghua.edu.cn/api/v2.1/share-links/{FILE_ID}/dirents/"
+DOWNLOAD_URL = f"https://cloud.tsinghua.edu.cn/d/{FILE_ID}/files/"
+
+total_file_ls = []
+
+def get_dirents(file_path: str=""):
+    payload = { 'thumbnail_size': 48,
+                'path': '/' + file_path.strip('/')}
+
+    r = requests.get(URL, params=payload)
+    content = json.loads(r.text)
+
+    return content['dirent_list']
+
+
+def make_path(path):
+    downloads_path = "Downloads/" + path.strip('/') + '/'
+    if not os.path.exists(downloads_path):
+        os.makedirs(downloads_path)
+    
+    return downloads_path
+
+
+def gen_file_ls(root_path: str=""):
+    global total_file_ls
+
+    file_ls = get_dirents(root_path)
+
+    folders = [i['folder_path'] for i in file_ls if i['is_dir']]
+    files = [i['file_path'] for i in file_ls if not i['is_dir']]
+
+    total_file_ls += files
+
+    if folders:
+        for i in folders:
+            gen_file_ls(i)
+
+
+def remove_ignores():
+    ignores = [f"/{i.lstrip('/')}" for i in IGNORE_LS]
+    
+    remove_ls = []
+    for i in ignores:
+        if not i.endswith('/') and i in total_file_ls:
+            remove_ls.append(i)
+        else:
+            for j in total_file_ls:
+                if j.startswith(i):
+                    remove_ls.append(j)
+    
+    return [i for i in total_file_ls if i not in remove_ls]
+
+
+def download(name: str, folder: str):
+    print(f"downloading {name}")
+    payloads = { 'p': folder + name,
+                 'dl': 1}
+
+    r = requests.get(DOWNLOAD_URL, params=payloads, stream=True)
+    download_path = make_path(folder)
+
+    with open(download_path + name, "wb") as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+
+
+def get_download_ls():
+    gen_file_ls()
+    download_ls = remove_ignores()
+    return download_ls
+
+
+def download_all(download_ls):
+    for i in download_ls:
+        name = i.split('/')[-1]
+        path = "/".join(i.split('/')[:-1]) + '/'
+        download(name, path)
+
+
+if __name__ == "__main__":
+    download_all(get_download_ls())
